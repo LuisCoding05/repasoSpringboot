a5dd8a923a5b0b85ac8193db5920c93c2194ddf8
LuisCoding05 <162503462+LuisCoding05@users.noreply.github.com.>
2025-03-13 14:01:11 +0100

    changes on Developer Entity

--- src/main/java/com/example/repaso/model/Developer.java
+++ src/main/java/com/example/repaso/model/Developer.java
@@ -1,5 +1,28 @@
 package com.example.repaso.model;
 
-public class Developer {
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+public class Developer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "foundation")
+    private Integer foundation;
+    
+    @OneToMany(mappedBy = "developer")
+    private Set<Videogame> juegos = new HashSet<>();
 }
